e60e88213856ac844e90b30c632140a60762cf7c
Mamadoubarry10 <Mamadou.barry1105@gmail.com>
2021-06-07 17:55:09 -0400

    anagram practice

---
+++ anagram.js
@@ -0,0 +1,30 @@
+
+function validAnagram(str, str2){
+    // add whatever parameters you deem necessary - good luck!
+    if(str.length !== str2.length){
+        return false
+    }
+    let obj = {}
+    
+    for(const el of str){
+       let counter = el
+       
+       obj[counter] ? obj[counter] += 1 : obj[counter] = 1 
+       
+    }
+    
+    for(const el of str2){
+        if( !obj[el] ){
+         
+            return false
+        
+        }else{
+            obj[el] -=1
+        }
+    }
+    console.log(obj)
+    
+  }
+
+
+  validAnagram('anagram', 'nagaram')
